e992ee46731fb53d53dad01851cba47068464e9b
mostafashraf18 <ma2963629@gmail.com>
2023-04-25 06:24:29 +0200

    fix headers

--- main.h
+++ main.h
@@ -21,6 +21,17 @@ typedef struct fmt
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_op(const char *format, fmt_t *print_arr, va_list list);
+int ch(va_list character);
+int str(va_list string);
+int _int(va_list integ);
+int _ui(va_list unsign);
+int _oct(va_list octo);
+int _hex_str(unsigned int n, unsigned int hex, char alpha);
+int _hex_l(va_list hexa);
+int _hex_u(va_list hexa);
+int _strlen(char *s);
+int _bin(va_list bin);
+int _putchar(char c);
 
 
 #endif
