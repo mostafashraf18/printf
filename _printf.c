f9f5aec31517578277d1f404efb863b81940c7af
mostafashraf18 <ma2963629@gmail.com>
2023-04-25 18:57:32 +0200

    version 3

--- _printf.c
+++ _printf.c
@@ -1,85 +1,73 @@
 #include "main.h"
-
-
 /**
- * deallocate - cleanup memory
- * @args: arguments
- * @output: struct of the container
- */
-void deallocate(va_list args, container_s *output)
-{
-	va_end(args);
-	write(1, output->start, output->size);
-	dealocate_container(output);
-}
-
-/**
- * _printf_helper - helper function
- * @format: string to print
- * @output: struct containing a string.
- * @args: list of arguments.
- * Return: The number of characters stored to output.
+* _printf - prints out string and arguments
+* @format: string to print
+* Return: size of string -1 if fail
 */
-
-int _printf_helper(const char *format, va_list args, container_s *output)
-{
-	int i, width, prec, rest = 0;
-	char tmp;
-	unsigned char flags, lenth;
-	unsigned int (*f)(container_s *, va_list,
-			unsigned char, int, int, unsigned char);
-
-	for (i = 0; *(format + i); i++)
-	{
-		lenth = 0;
-		if (*(format + i) == '%')
-		{
-			tmp = 0;
-			flags = handleflag(format + i + 1, &tmp);
-			width = handlewidth(args, format + i + tmp + 1, &tmp);
-			prec = handleprec(args, format + i + tmp + 1,
-					&tmp);
-			lenth = handlelen(format + i + tmp + 1, &tmp);
-
-			f = handle_specifiers(format + i + tmp + 1);
-			if (f != NULL)
-			{
-				i += tmp + 1;
-				rest += f(output, args, flags, width, prec, lenth);
-				continue;
-			}
-			else if (*(format + i + tmp + 1) == '\0')
-			{
-				rest = -1;
-				break;
-			}
-		}
-		rest += _copy(output, (format + i), 1);
-		i += (lenth != 0) ? 1 : 0;
-	}
-	deallocate(args, output);
-	return (rest);
-
-}
-/**
- * _printf - Print astring.
- * @format: contains directives.
- * Return: The number of characters printed.
- */
 
 int _printf(const char *format, ...)
 {
-	container_s *output;
-	va_list args;
-	int ret = 0;
+	int i, pc = 0, p = 0;
+	va_list list;
 
-	if (format == NULL)
-		return (-1);
-	output = init_buffer();
-	if (init_buffer() == NULL)
-		return (-1);
-	va_start(args, format);
-	ret = _printf_helper(format, args, output);
 
-	return (ret);
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			pc++;
+		}
+		else
+		{
+			i++;
+			if (format[i])
+				p = handle_print(format, &i, list);
+			else
+				p = -1;
+			if (p == -1 && format[i])
+			{
+				print_37(list);
+				pc++;
+				i--;
+			}
+			else if (p == -1)
+				return (-1);
+			else if (p != -1)
+				pc += p;
+		}
+	}
+	va_end(list);
+	return (pc);
+}
+
+/**
+ * handle_print - takes care of printing
+ * @format: pointer to format string
+ * @i: index of char
+ * @list: arguments
+ * Return: size of print or -1 if fail
+ */
+
+
+int handle_print(const char *format, int *i, va_list list)
+{
+	ff function[] = {
+		{'c', print_char}, {'s', print_str},
+		{'%', print_37}, {'i', print_int},
+		{'d', print_int}, {'\0', NULL}
+	};
+	int j;
+
+	for (j = 0; j < 6; ++j)
+	{
+		if (function[j].c == format[*i])
+			return (function[j].print(list));
+	}
+	return (-1);
 }
--- main.h
+++ main.h
@@ -1,116 +1,42 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
-#include <stdarg.h>
+#define BUFF_SIZE 1024
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 
-#define PLUS 1
-#define SPACE 2
-#define HASH 4
-#define ZERO 8
-#define NEG 16
-#define PLUS_FLAG (flags & 1)
-#define SPACE_FLAG ((flags >> 1) & 1)
-#define HASH_FLAG ((flags >> 2) & 1)
-#define ZERO_FLAG ((flags >> 3) & 1)
-#define NEG_FLAG ((flags >> 4) & 1)
-
-#define SHORT 1
-#define LONG 2
+/**
+* struct flags - struct containing pointers to functions
+*
+* @c: format
+* @print: pointer to function assosciated
+*/
+struct flags
+{
+	char c;
+	int (*print)(va_list);
+};
 
 /**
- * struct container - astruct.
- * @container: pointer to astring
- * @start: pointer to the start of the string
- * @size: The length of the string
- */
-typedef struct container
-{
-	char *container;
-	char *start;
-	unsigned int size;
-} container_s;
+* typedef struct flags ff - struct definition
+*
+* @flags: format
+* @ff: function assosciated
+*/
 
-/**
- * struct flag_s -flags struct.
- * @flag: aflag.
- * @value: The value of the flag.
- */
-typedef struct flag_s
-{
-	unsigned char flag;
-	unsigned char value;
-} array_f;
+typedef struct flags ff;
 
-/**
- * struct linker - A linker struct.
- * @specifier: A specifier
- * @_func: linker tothe right function
- */
+int _printf(const char *form, ...);
+int _strlen(char *s);
 
-typedef struct linker
-{
-	unsigned char specifier;
-unsigned int (*_func)(container_s *, va_list,
-			unsigned char, int, int, unsigned char);
-} linker_l;
-
-int _printf(const char *format, ...);
-
-/*linker functions*/
-unsigned int link_c(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_s(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_di(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_percent(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_b(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_u(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_o(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_x(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_X(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_S(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_p(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_r(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int link_R(container_s *string, va_list args,
-		unsigned char flags, int wid, int prec, unsigned char len);
-
-/*helpers*/
-
-container_s *init_buffer(void);
-void dealocate_container(container_s *container);
-unsigned int _copy(container_s *container, const char *src, unsigned int n);
-unsigned int linker_sbase(container_s *container, long int no, char *base,
-		unsigned char flags, int wid, int prc);
-unsigned int linker_ubase(container_s *container, unsigned long int no,
-char *base, unsigned char flags, int wid, int prc);
-
-
-unsigned char handleflag(const char *flags, char *index);
-unsigned char handlelen(const char *modifier, char *index);
-int handlewidth(va_list args, const char *modifier, char *index);
-int handleprec(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(container_s *, 
-va_list, unsigned char, int, int, unsigned char);
-
-
-unsigned int get_width(container_s *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int getstring_width(container_s *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int getneg_width(container_s *output, unsigned int printed,
-		unsigned char flags, int wid);
-
+/* Funtions to print chars and strings */
+int handle_print(const char *format, int *i, va_list list);
+int print_char(va_list list);
+int print_str(va_list list);
+int _putchar(char c);
+int print_37(va_list list);
+/* Functions to print numbers */
+int print_int(va_list list);
 #endif
