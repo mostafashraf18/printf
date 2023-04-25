ed49ba34f89b3403376b33c94b8b2272fe650bff
mostafashraf18 <ma2963629@gmail.com>
2023-04-25 18:12:05 +0200

    s6

--- get_width.c
+++ get_width.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * get_width - Stores leading spaces to a buffer for a width modifier
+ * get_width - Stores Leading spaces to a buffer for a width modifier
  * @output: Astruct
  * @printed: The number of characters already printed
  * @flags: Flag modifiers
