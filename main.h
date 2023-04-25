7682fea5e294010af42679ab5bc56914bbff36ab
mostafashraf18 <ma2963629@gmail.com>
2023-04-25 17:01:17 +0200

    version 1

--- main.h
+++ main.h
@@ -1,37 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
-
-
-/**
- *  struct fmt - function to check for formats
- *  @type: The format to printf 
- *  @f: The print function
- */
-typedef struct fmt
-{
-	char *type;
-	int (*f)();
-} fmt_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_op(const char *format, fmt_t *print_arr, va_list list);
-int ch(va_list character);
-int str(va_list string);
-int _int(va_list integ);
-int _ui(va_list unsign);
-int _oct(va_list octo);
-int _hex_str(unsigned int n, unsigned int hex, char alpha);
-int _hex_l(va_list hexa);
-int _hex_u(va_list hexa);
-int _strlen(char *s);
-int _bin(va_list bin);
-int _putchar(char c);
-
+int _puts(char *string);
+int _puts_digit(int num);
+int process_flag(char specifier, va_list all_parameters);
+int is_flag(char percent, char specifier);
 
 #endif
